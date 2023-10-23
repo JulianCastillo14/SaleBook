5c98a4680ea01b4b1aab36b4c7d1bfaa72573c48
JuanGaravito <juang12162002@gmail.com>
2023-10-23 16:49:07 -0500

    Conexion Base de Datos y Backend Libros

--- src/main/java/com/SaleBook/backend/controlador/LibroController.java
+++ src/main/java/com/SaleBook/backend/controlador/LibroController.java
@@ -4,10 +4,76 @@
  */
 package com.SaleBook.backend.controlador;
 
+import com.SaleBook.backend.modelo.Libro;
+import com.SaleBook.backend.servicio.LibroServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author juang
  */
+@RestController
+@CrossOrigin("*")
+@RequestMapping("api/libros")
 public class LibroController {
+    @Autowired
+    private LibroServicio libroServicio;
     
+    @GetMapping("/list")
+    public List<Libro> consultarTodo(){
+        return libroServicio.getLibros();
+    }
+    
+    @GetMapping("/list/{id_libro}")
+    public Libro buscarId(@PathVariable("id_libro") String isbn){
+        return libroServicio.getLibro(isbn);
+    }
+    
+    @PostMapping("/")
+    public ResponseEntity<Libro> agregar(@RequestBody Libro libro){
+        Libro obj = libroServicio.grabarLibro(libro);
+        return new ResponseEntity<>(obj,HttpStatus.OK);
+    }
+    
+    @PutMapping("/")
+    public ResponseEntity<Libro> editar(@RequestBody Libro libro){
+        Libro obj = libroServicio.getLibro(libro.getIsbn());
+        if(obj != null){
+            obj.setTitulo(libro.getTitulo());
+            obj.setAutor(libro.getAutor());
+            obj.setEdicion(libro.getEdicion());
+            obj.setCategoria(libro.getCategoria());
+            obj.setFecha_publicacion(libro.getFecha_publicacion());
+            obj.setEditorial(libro.getEditorial());
+            obj.setIdioma(libro.getIdioma());
+            obj.setValor_unitario(libro.getValor_unitario());
+            obj.setStock(libro.getStock());
+        }else{
+            return new ResponseEntity<>(obj,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(obj,HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}")
+     public ResponseEntity<Libro> eliminar(@PathVariable String isbn){
+        Libro obj = libroServicio.getLibro(isbn);
+        if(obj != null){
+           libroServicio.deleteLibro(isbn);
+        }else{
+           return new ResponseEntity<>(obj,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(obj,HttpStatus.OK);
+    }
 }
--- src/main/java/com/SaleBook/backend/servicio/ILibroServicio.java
+++ src/main/java/com/SaleBook/backend/servicio/ILibroServicio.java
@@ -2,15 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.SaleBook.backend.repositorio;
+package com.SaleBook.backend.servicio;
 
 import com.SaleBook.backend.modelo.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 /**
  *
  * @author juang
  */
-public interface LibroRepositorio extends JpaRepository<Libro, String>{
+public interface ILibroServicio {
+    public List<Libro> getLibros();
     
+    public Libro getLibro(String isbn);
+    
+    public Libro grabarLibro(Libro libro);
+    
+    public void deleteLibro(String isbn);
 }
