5bc944df423444ab0125e42e7fed9973a0284252
JulianCastillo14 <juliancast140502@gmail.com>
2023-11-24 16:47:47 -0500

    Consumiendo el backend ahora con un frontend desarrollado en react

--- SaleBook - React/src/App.jsx
+++ SaleBook - React/src/App.jsx
@@ -3,7 +3,7 @@ import {Catalogo} from "./componentes/Catalogo"
 import { BrowserRouter , Route, Routes } from 'react-router-dom'; 
 import { Detalles } from "./componentes/Detalles";
 import {Carrito} from "./componentes/Carrito"
-import {Administracion} from "./screens/Administracion.jsx"
+import Administracion from "./screens/Administracion.jsx"
 import {LoginAdministracion } from "./componentes/LoginAdministracion.jsx";
 
 export function App() {
@@ -11,11 +11,11 @@ export function App() {
     <BrowserRouter>
       {/* <Header></Header> */}
       <Routes>
-          <Route path="/" element={<Catalogo/>} /> 
+          {/* <Route path="/" element={<Catalogo/>} />  */}
           <Route path="/detalles/:isbn" element={<Detalles/>} />
           <Route path="/carrito" element={<Carrito/>} />
           <Route path="/Administracion" element={<Administracion/>}/>
-          <Route path="/loginadministracion" element={<LoginAdministracion/>}/>
+          <Route path="/" element={<LoginAdministracion/>}/>
       </Routes>
     </BrowserRouter> 
   )
--- SaleBook - React/src/componentes/EditarEmpleado.jsx
+++ SaleBook - React/src/componentes/EditarEmpleado.jsx
@@ -4,30 +4,61 @@ import { Peticion } from '../js/Peticion';
 
 const EditarEmpleado = () => {
     const [form, setForm] = useState({
-        Documento: '',
-        'Primer-nombre': '',
-        'Segundo-nombre': '',
-        'Primer-apellido': '',
-        'Segundo-apellido': '',
-        'Tipo-Documento': '',
-        'Fecha-nacimiento': '',
-        Genero: '',
-        Correo: '',
-        Usuario: '',
-        Password: ''
+        primerNombre: '',
+        segundoNombre: '',
+        primerApellido: '',
+        segundoApellido: '',
+        numeroDocumento: '',
+        tipoDocumento: '',
+        fechaNacimiento: '',
+        genero: '',
+        correo: '',
+        usuario: '',
+        password: ''
     });
 
-    const handleChange = (event) => {
+    const handleChange = (e) => {
         setForm({
             ...form,
-            [event.target.id]: event.target.value
+            [e.target.name]: e.target.value
         });
     };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const {status, respuesta: Empleados } = await Peticion("http://localhost:2020/api/Empleados/","PUT", form);
-        if(status){
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const tipoDocumentoMap = {
+            "TI": {
+                "idTipoDocumento": 1,
+                "abreviatura": "TI",
+                "descripcion": "TARJETA DE IDENTIDAD"
+            },
+            "CC": {
+                "idTipoDocumento": 2,
+                "abreviatura": "CC",
+                "descripcion": "CEDULA DE CIUDADANIA"
+            },
+            "TE": {
+                "idTipoDocumento": 3,
+                "abreviatura": "TE",
+                "descripcion": "TARJETA DE EXTRANJERIA"
+            },
+            "CE": {
+                "idTipoDocumento": 4,
+                "abreviatura": "CE",
+                "descripcion": "CEDULA DE EXTRANJERIA"
+            }
+        };
+
+        const data = {
+            ...form,
+            idTipoDocumento: tipoDocumentoMap[form.tipoDocumento],
+            fechaNacimiento: new Date(form.fechaNacimiento).toISOString().split('T')[0]
+        };
+
+        const { status } = await Peticion("http://localhost:2020/api/Empleados/", "PUT", data);
+
+        if (status) {
             alert("Empleado editado correctamente");
         } else {
             alert("Ocurrió un error al editar el empleado");
@@ -35,48 +66,44 @@ const EditarEmpleado = () => {
     };
 
     return (
-        <div id='container-ediEmpleado'>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <input className='input-ede' type="text" id="Documento" name="nombre" placeholder=" Documento" value={form.Documento} onChange={handleChange}/>
-                <input className='input-ede' id="Primer-nombre" type="text" placeholder=" Primer nombre" value={form['Primer-nombre']} onChange={handleChange}/>
-                <input className='input-ede' id="Segundo-nombre" type="text" placeholder=" Segundo nombre" value={form['Segundo-nombre']} onChange={handleChange}/>
-                <input className='input-ede' id="Primer-apellido" type="text" placeholder=" Primer apellido" value={form['Primer-apellido']} onChange={handleChange}/>
-            </div>
-            <div>
-                <input className='input-ede' id="Segundo-apellido" type="text" placeholder=" Segundo apellido" value={form['Segundo-apellido']} onChange={handleChange}/>
-                <select className='select-ede' id="Tipo-Documento" value={form['Tipo-Documento']} onChange={handleChange}>
-                    <option value="" disabled selected>Selecciona un tipo de documento</option>
-                    <option value="TI">Tarjeta de identidad</option>
-                    <option value="CC">Cédula de ciudadanía </option>
-                    <option value="TE">Tarjeta de extranjería</option>
-                    <option value="CE">Cédula de extranjería </option>
-                </select>
-                <div id="Fecha-Nacimiento">
-                    Fecha de Nacimiento:
-                    <input className='input-ede' id="Fecha-nacimiento" type="date" value={form['Fecha-nacimiento']} onChange={handleChange}/>
+        <section id="container-ediEmpleado">
+            <form onSubmit={handleSubmit}>
+                <div className="ajustar-div-ede">
+                    <input name="primerNombre" type="text" placeholder=" Primer nombre" onChange={handleChange} className="input-ede" />
+                    <input name="segundoNombre" type="text" placeholder=" Segundo nombre" onChange={handleChange} className="input-ede" />
+                    <input name="primerApellido" type="text" placeholder=" Primer apellido" onChange={handleChange} className="input-ede" />
+                    <input name="segundoApellido" type="text" placeholder=" Segundo apellido" onChange={handleChange} className="input-ede" />
                 </div>
-            </div>
-            <div className='ajustar-div-ede'>
-                <select className='select-ede' id="Genero" name="genero" value={form.Genero} onChange={handleChange}>
-                    <option value="" disabled selected>Selecciona una identidad de género</option>
-                    <option value="Masculino">Masculino</option>
-                    <option value="Femenino">Femenino</option>
-                </select>
-                <input className='input-ede' id="Correo" type="email" placeholder=" Correo electrónico" value={form.Correo} onChange={handleChange}/>
-            </div>
-            <div className='ajustar-div-ede'>
-                <input className='input-ede' id="Usuario" type="text" placeholder=" Usuario" value={form.Usuario} onChange={handleChange}/>
-                <input className='input-ede' id="Password" type="text" placeholder=" Contraseña" value={form.Password} onChange={handleChange}/>
-                <button className="btn-editar" type="submit">Editar Empleado</button>
-            </div>
-        </form>
-    </div>
+                <div className="ajustar-div-ede">
+                    <input name="numeroDocumento" type="text" placeholder=" Documento" onChange={handleChange} className="input-ede" />
+                    <select name="tipoDocumento" onChange={handleChange} className="select-ede">
+                        <option value="" disabled selected>Selecciona un tipo de documento</option>
+                        <option value="TI">Tarjeta de identidad</option>
+                        <option value="CC">Cédula de ciudadanía </option>
+                        <option value="TE">Tarjeta de extranjería</option>
+                        <option value="CE">Cédula de extranjería </option>
+                    </select>
+                    <div id="Fecha-Nacimiento">
+                        Fecha de Nacimiento:
+                        <input name="fechaNacimiento" type="date" onChange={handleChange} className="input-ede" />
+                    </div>
+                </div>
+                <div className="ajustar-div-ede">
+                    <select name="genero" onChange={handleChange} className="select-ede">
+                        <option value="" disabled selected>Selecciona una identidad de género</option>
+                        <option value="Masculino">Masculino</option>
+                        <option value="Femenino">Femenino</option>
+                    </select>
+                    <input name="correo" type="email" placeholder=" Correo electrónico" onChange={handleChange} className="input-ede" id="Correo" />
+                </div>
+                <div className="ajustar-div-ede">
+                    <input name="usuario" type="text" placeholder=" Usuario" onChange={handleChange} className="input-ede" />
+                    <input name="password" type="text" placeholder=" Contraseña" onChange={handleChange} className="input-ede" />
+                    <button type="submit" className="btn-editar">Editar Empleado</button>
+                </div>
+            </form>
+        </section>
     );
-};
+}
 
 export default EditarEmpleado;
-
-
-
-
